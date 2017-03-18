9f3ea1984db5be198d2c47ad1648fd7c5baa0d70
unknown <jinxian.dou@office.51fanli.com>
2017-03-18 13:56:10 +0800

    new

--- src/com/lijh/GeneratoMain.java
+++ src/com/lijh/GeneratoMain.java
@@ -7,7 +7,7 @@ public class GeneratoMain {
 		  GeneratorFacade g = new GeneratorFacade();
 	    
 	        g.deleteOutRootDir();   //删除生成器的输出目录
-             g.generateByTable("tb_tuan_product_audit_log","template");     //通过数据库表生成文件,template为模板的根目录
+             g.generateByTable("t_car_workorder","template");     //通过数据库表生成文件,template为模板的根目录
 //	        g.generateByAllTable("template");       //自动搜索数据库中的所有表并生成文件,template为模板的根目录
 //	              g.generateByClass(Blog.class,"template_clazz");
 	        
